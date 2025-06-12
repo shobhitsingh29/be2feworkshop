5c883913177c39704bc516ba240df64fee5700a7
Philipp Gieschen <philipp.gieschen@zalando.de>
2025-06-12 12:14:52 +0200

    service skeleton

---
+++ src/main/kotlin/de/phgieschen/fe2be/snapshot/SnapshotApplication.kt
@@ -0,0 +1,11 @@
+package de.phgieschen.fe2be.snapshot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnapshotApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnapshotApplication>(*args)
+}
