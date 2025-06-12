7c1330fdbdc66eee7d9bd07145d15f6bfb4cea6b
Philipp Gieschen <philipp.gieschen@zalando.de>
2025-06-12 12:19:08 +0200

    get sample endpoint with path variable

--- src/main/kotlin/de/phgieschen/fe2be/snapshot/controller/SampleController.kt
+++ src/main/kotlin/de/phgieschen/fe2be/snapshot/controller/SampleController.kt
@@ -3,6 +3,7 @@ package de.phgieschen.fe2be.snapshot.controller
 import de.phgieschen.fe2be.snapshot.dto.ResponseDTO
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,5 +12,12 @@ class SampleController {
     @GetMapping("/sample")
     fun getSample(): ResponseEntity<ResponseDTO> {
         return ResponseEntity.ok(ResponseDTO(status = 201, message = "Sample message"))
+    }
+
+    @GetMapping("/sample/{message}")
+    fun getSampleMessage(
+        @PathVariable message: String
+    ): ResponseEntity<ResponseDTO> {
+        return ResponseEntity.ok(ResponseDTO(status = 201, message = message))
     }
 }
